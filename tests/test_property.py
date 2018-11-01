61a1d655597f6b34a0c1a7c783c62c83c35efbc2
Yernar Kambar <ernar.k.20@gmail.com>
2018-11-01 21:44:21 +0600

    created 3 separate files for tests and 3 for solutions

--- tests/test_property.py
+++ tests/test_property.py
@@ -1,21 +1,8 @@
 from unittest import TestCase
-from classmethods_staticmethods_property import Employee as emp
+from property import Employee as emp
 
 
-class ClassmethodsStaticmethodsPropertyTest(TestCase):
-
-    def test_classmethod_create_new_employee(self):
-        """
-        Dear bratishka or sestrenka, this test checks your solution in classmethod-create_new_employee
-        To pass this test you need true solution in classmethod-create_new_employee
-        new_employee type: instance of Employee class
-        """
-        new_employee = emp.create_new_employee('Edmon-Dantes-10000000')
-        self.assertEqual(True, isinstance(new_employee, emp))
-
-    def test_staticmethod_calculate_salary_in_year(self):
-        sal = emp.calculate_salary_in_year(5);
-        self.assertEqual(sal, 60)
+class PropertyTest(TestCase):
 
     def test_property_getter_fullname_of_employee(self):
         new_employee = emp('Emantsrif', 'Emantsal', 422000)
